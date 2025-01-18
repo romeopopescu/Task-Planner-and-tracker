28c28d76a40f6187b9b2e79260682dc2c70f8b5e
Romeo <popescuromeo22@stud.ase.ro>
2025-01-18 14:22:36 +0200

    added sequelize

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,24 @@
-const Pool = require('pg').Pool;
+// const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"task_planner_db",
-    password:"1234",
-    port:5432,
-});
+// const pool = new Pool({
+//     user:"postgres",
+//     host:"localhost",
+//     database:"task_planner_db",
+//     password:"1234",
+//     port:5432,
+// });
 
-module.exports = pool;
+// module.exports = pool;
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://localhost:5432/task_planner_db');
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+module.exports = sequelize;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const usersRoute = require('./src/user/routes');
+const sequelize = require('./db');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
+
+
 
 app.use('/api/v1/users', usersRoute);
 
